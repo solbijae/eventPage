7d4fb3b9350f4bf0b8641da8026d25948181ac69
jaeyeonjung <jaeyeonjung3@gmail.com>
2025-01-03 16:44:01 +0900

    [style] change default font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">test</div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import mediaQueries from './styles/MediaQuery';
+import { ThemeConfig } from './styles/ThemeConfig';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ThemeProvider theme={{ ...ThemeConfig, mediaQueries }}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
